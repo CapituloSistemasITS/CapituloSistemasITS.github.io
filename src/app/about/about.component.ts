b86e492514f59bab239c7fcb5ca4ee11337b84f5
LEONARDO CONTRERAS MARTINEZ <L21051403@saltillo.tecnm.mx>
2024-03-17 20:04:12 -0600

    Fixed a bug with large screens where the about container wont load up according to its content

--- src/app/about/about.component.ts
+++ src/app/about/about.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, ViewChildren, ElementRef, AfterViewInit, QueryList } from '@angular/core';
 import { MiembrosComponent } from './miembros/miembros.component';
 declare var AOS: any;
 
@@ -9,10 +9,42 @@ declare var AOS: any;
   templateUrl: './about.component.html',
   styleUrl: './about.component.scss'
 })
-export class AboutComponent implements OnInit {
+export class AboutComponent implements OnInit, AfterViewInit {
+  @ViewChildren('aosElement') aosElements!: QueryList<ElementRef>;
+  private observer: MutationObserver | undefined;
+
   ngOnInit() {
     AOS.init();
   }
+
+  ngAfterViewInit() {
+    this.observer = new MutationObserver(() => {
+      this.resizeContainer();
+    });
+
+    this.aosElements.forEach(element => {
+      if (this.observer) {
+        this.observer.observe(element.nativeElement, { attributes: true, childList: true, subtree: true });
+      }
+    });
+  }
+
+  resizeContainer() {
+    const container: HTMLElement | null = document.querySelector('.wrapper');
+
+    if (container) {
+      let totalHeight = 0;
+      this.aosElements.forEach(element => {
+        totalHeight += element.nativeElement.offsetHeight;
+      });
+      console.log(totalHeight);
+      container.style.minHeight = `${totalHeight}px`;
+    }
+  }
+
+  ngOnDestroy() {
+    if (this.observer) {
+      this.observer.disconnect();
+    }
+  }
 }
-
-
